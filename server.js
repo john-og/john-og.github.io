c57f93f2a643dbdf6d84964facced1f18e85cd2a
John Ogundeji <jogundej@uwaterloo.ca>
2020-05-20 21:32:59 -0400

    server update

--- server.js
+++ server.js
@@ -5,15 +5,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-if (process.env.NODE_ENV) {
-    app.use((req, res, next) => {
-      if (req.header('x-forwarded-proto') !== 'https') {
-        res.redirect('https://' + req.hostname + req.url);
-      } else {
-        next()
-      }
-    })
-  }
+
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
